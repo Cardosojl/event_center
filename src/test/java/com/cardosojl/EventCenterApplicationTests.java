65e357fd5a878b9052176e9c60c67fde30c008ad
cardosojl <cardosojl@outlook.com.br>
2024-02-04 15:16:26 -0300

    init

---
+++ src/test/java/com/cardosojl/EventCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardosojl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
