c3a7de08f1f72d10171ccaebd0a2405589a0ae4c
cardosojl <cardosojl@outlook.com.br>
2024-04-13 15:21:16 -0300

    initial

--- src/main/java/com/cardosojl/Startup.java
+++ src/main/java/com/cardosojl/Startup.java
@@ -16,14 +16,7 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 		
-		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
-		encoders.put("pbkdf2", pbkdf2Encoder);
-		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
 		
-		String result = passwordEncoder.encode("123456");
-		System.out.println("My hash" + result);
 	}
 
 }
